f0a921547bf372783fce2fbb0533b504efbbdc1a
Barbara Kulawska <kulawskab@student.agh.edu.pl>
2022-03-20 02:05:13 +0100

    port settings

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -107,4 +109,5 @@ def internal_server_error(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
